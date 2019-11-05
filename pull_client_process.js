a4101346b92e81753976b90714149ae5699370d6
Simon Rogers <scriptorian@streampunk.media>
2019-11-05 15:01:32 +0000

    pull_client with support for processing and 10-bit macadam

---
+++ pull_client_process.js
@@ -0,0 +1,36 @@
+const wsl = require('ws');
+const macadam = require('macadam');
+const request = require('request-promise-native')
+
+//  let ws = new wsl('http://localhost:8080')
+let playback
+
+// console.dir(macadam.getDeviceInfo()[0].outputDisplayModes);
+
+let timer = (w) => new Promise((resolve) => {
+	setTimeout(resolve, w);
+})
+
+async function init () {
+  playback = await macadam.playback({
+	  deviceIndex: 0, // Index relative to the 'macadam.getDeviceInfo()' array
+	  displayMode: macadam.bmdModeHD1080i50,
+	  // pixelFormat: macadam.bmdFormat8BitBGRA
+	  pixelFormat: macadam.bmdFormat10BitYUV
+	});
+	// ws.on('message', m => {
+	// 	console.log(m.length);
+	// 	playback.displayFrame(m);
+	// });
+	let start = process.hrtime();
+	let counter = 0;
+	while (true) {
+		let frame = await request('http://localhost:3001/', { encoding: null });
+		playback.displayFrame(frame);
+		let end = process.hrtime(start);
+		let wait = (40000000 * counter - end[1]) / 1000000 | 0;
+		await timer(wait);
+	}
+};
+
+init();
